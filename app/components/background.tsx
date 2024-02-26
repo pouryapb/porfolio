31669e2cc995648d992638f9a674d67eb5546646
pouryapb <pouryapb77@gmail.com>
2024-02-26 22:13:02 +0330

    removed nyan cat

--- app/components/background.tsx
+++ app/components/background.tsx
@@ -1,15 +1,9 @@
-import Image from "next/image";
-
-import nyanCat from "@/public/nyan-cat.gif";
 import { BackgroundGradientAnimation } from "./ui/background-gradient-animation";
 
 export default function Background() {
   return (
     <>
       <BackgroundGradientAnimation />
-      <div className="absolute -bottom-10 -left-32">
-        <Image priority src={nyanCat} alt="nyanCat" className="animate-nyan" />
-      </div>
     </>
   );
 }
